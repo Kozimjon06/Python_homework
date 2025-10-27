e402998f8c8d6aee0a5dd2fa270fc5c152e2fd54
Kozimjon06 <kozimismoilov06@gmail.com>
2025-10-27 17:49:22 +0500

    Create hw1.py

---
+++ class1/homework/hw1.py
@@ -0,0 +1,30 @@
+# Homework Program
+
+# 1. Square
+side = float(input("Enter side of square: "))
+perimeter = 4 * side
+area = side * side
+print("Perimeter of square =", perimeter)
+print("Area of square =", area)
+
+# 2. Circle
+d = float(input("Enter diameter of circle: "))
+pi = 3.14
+length = pi * d
+print("Length of circle =", length)
+
+# 3. Mean of two numbers
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+mean = (a + b) / 2
+print("Mean =", mean)
+
+# 4. Sum, product and squares
+sum1 = a + b
+product = a * b
+square_a = a * a
+square_b = b * b
+print("Sum =", sum1)
+print("Product =", product)
+print("Square of first number =", square_a)
+print("Square of second number =", square_b)
